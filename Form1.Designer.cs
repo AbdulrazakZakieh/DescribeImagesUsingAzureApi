f3bd7adf633f338f2acd97be9bb894cc5e883406
AbdulrazakZakieh <42899275+AbdulrazakZakieh@users.noreply.github.com>
2019-12-22 07:43:37 +0300

    initial-commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿namespace DescripeImagesUsingAPIs
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.uploadButton = new System.Windows.Forms.Button();
+			this.descriptionLabel = new System.Windows.Forms.Label();
+			this.imagePictureBox = new System.Windows.Forms.PictureBox();
+			((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).BeginInit();
+			this.SuspendLayout();
+			// 
+			// uploadButton
+			// 
+			this.uploadButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.uploadButton.Location = new System.Drawing.Point(11, 329);
+			this.uploadButton.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.uploadButton.Name = "uploadButton";
+			this.uploadButton.Size = new System.Drawing.Size(156, 38);
+			this.uploadButton.TabIndex = 0;
+			this.uploadButton.Text = "Upload";
+			this.uploadButton.UseVisualStyleBackColor = true;
+			this.uploadButton.Click += new System.EventHandler(this.Button1_Click);
+			// 
+			// descriptionLabel
+			// 
+			this.descriptionLabel.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+			this.descriptionLabel.AutoSize = true;
+			this.descriptionLabel.Location = new System.Drawing.Point(186, 340);
+			this.descriptionLabel.Name = "descriptionLabel";
+			this.descriptionLabel.Size = new System.Drawing.Size(0, 17);
+			this.descriptionLabel.TabIndex = 1;
+			// 
+			// imagePictureBox
+			// 
+			this.imagePictureBox.Location = new System.Drawing.Point(11, 4);
+			this.imagePictureBox.Name = "imagePictureBox";
+			this.imagePictureBox.Size = new System.Drawing.Size(599, 319);
+			this.imagePictureBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+			this.imagePictureBox.TabIndex = 2;
+			this.imagePictureBox.TabStop = false;
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(622, 379);
+			this.Controls.Add(this.imagePictureBox);
+			this.Controls.Add(this.descriptionLabel);
+			this.Controls.Add(this.uploadButton);
+			this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+			this.Name = "Form1";
+			this.Text = "Form1";
+			((System.ComponentModel.ISupportInitialize)(this.imagePictureBox)).EndInit();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button uploadButton;
+		private System.Windows.Forms.Label descriptionLabel;
+		private System.Windows.Forms.PictureBox imagePictureBox;
+	}
+}
+
---
+++ Form1.cs
@@ -0,0 +1,72 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DescripeImagesUsingAPIs
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+		/// <summary>
+		/// Read the bytes of a specific image
+		/// </summary>
+		/// <param name="imagePath"></param>
+		/// <returns></returns>
+		private byte[] getImageBytes(string imagePath)
+		{
+			using (FileStream fileStream = new FileStream(imagePath, FileMode.Open))
+			{
+				BinaryReader reader = new BinaryReader(fileStream);
+				return reader.ReadBytes((int)fileStream.Length);
+			}
+		}
+		private void Button1_Click(object sender, EventArgs e)
+		{
+			OpenFileDialog openFileDialog = new OpenFileDialog();
+			openFileDialog.Filter = "PNG Images (*.png)|*.png|JPEG Images (*.jpeg)|*.jpeg" +
+				"|GIF Images (*.gif)|*.gif|BMP Images (*.bmp)|*.bmp";
+			if (openFileDialog.ShowDialog() == DialogResult.OK)
+			{
+				string imagePath = openFileDialog.FileName;
+				byte[] imageBytes = getImageBytes(imagePath);
+				imagePictureBox.Image = Image.FromFile(imagePath);
+				// Defining new client
+				HttpClient httpClient = new HttpClient();
+				// Adding a parameter to the http request header (The subscription key: valid till 27. Dec. 2019)
+				httpClient.DefaultRequestHeaders.Add(
+					"ocp-apim-subscription-key", // parameter name
+					"814d4923ad0e4ba3818884cf8df10c9a"); // parameter value
+				// Defining the content for the http post body (the image's bytes as specified by the API's documentation)
+				ByteArrayContent content =
+					new ByteArrayContent(imageBytes);
+				// specifiying the content type (From the API's documention)
+				content.Headers.ContentType =
+					new MediaTypeHeaderValue("application/octet-stream");
+				HttpResponseMessage httpResponse =
+					httpClient.PostAsync(
+					"https://westcentralus.api.cognitive.microsoft.com/vision/v2.0/analyze?" +
+					"visualFeatures=Description",
+					content
+					).Result;
+				string responseString = httpResponse.Content.ReadAsStringAsync().Result;
+				dynamic x =
+					JsonConvert.DeserializeObject<dynamic>(responseString);
+				string text = x.description.captions[0].text;
+				descriptionLabel.Text = text;
+			}
+		}
+    }
+}
